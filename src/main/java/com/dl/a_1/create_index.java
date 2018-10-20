6203e24c67a5bca6eda18056875d290fc30e56f5
Daniel Lavelle <daniellavelle@Daniels-MacBook-Pro.local>
2018-10-20 17:58:59 +0100

    Initial commmit

---
+++ src/main/java/com/dl/a_1/create_index.java
@@ -0,0 +1,161 @@
+package com.dl.a_1;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class create_index {
+    private static String INDEX_DIRECTORY = "index";
+    public static void main(String[] args) throws IOException, ParseException {
+
+        //--------------- SET UP INDEX ----------------------
+        //StandardAnalyzer analyzer = new StandardAnalyzer();
+        //StopAnalyzer analyzer = new StopAnalyzer();
+        //SimpleAnalyzer analyzer = new SimpleAnalyzer();
+        //WhitespaceAnalyzer analyzer = new WhitespaceAnalyzer();
+        EnglishAnalyzer analyzer = new EnglishAnalyzer();
+        Directory index = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
+
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        IndexWriter w = new IndexWriter(index, config);
+
+        read_cran_docs(w);
+
+
+        w.close();
+
+        
+       
+    }
+
+    private static void addDoc(IndexWriter w, Map <String, String> doc) throws IOException {
+        //Enter hashmap as document
+        Document d = new Document();
+        d.add(new StringField(("id"), doc.get("I"), Field.Store.YES));
+       
+
+        try{
+            d.add(new StringField(("author"), doc.get("A"), Field.Store.YES));
+        } catch(IllegalArgumentException e){
+            d.add(new StringField(("author"), "", Field.Store.YES));
+        }
+
+        try{
+            d.add(new TextField(("bibliography"), doc.get("B"), Field.Store.YES));
+        } catch(IllegalArgumentException e){
+            d.add(new TextField(("bibliography"), "", Field.Store.YES));
+        }
+
+        try{
+            d.add(new StringField(("title"), doc.get("T"), Field.Store.YES));
+        } catch(IllegalArgumentException e){
+            d.add(new StringField(("title"), "", Field.Store.YES));
+        }
+
+        try {
+            d.add(new TextField(("words"), doc.get("W"), Field.Store.YES));
+        } catch (IllegalArgumentException e) {
+            d.add(new TextField(("words"), "", Field.Store.YES));
+        }
+        w.addDocument(d);
+        
+        
+    }
+
+    private static void read_cran_docs(IndexWriter w) throws IOException {
+        //Open file
+        String cran_direc = "/Users/daniellavelle/Documents/MAI/CS7IS3/a_1/cran/cran_docs";
+        FileInputStream fstream = new FileInputStream(cran_direc);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+        String strLine, current_mode, current_entry;
+        current_mode = "I";
+        Map<String, String> doc = new HashMap<String, String>();
+
+        //Read File Line By Line
+        while ((strLine = br.readLine()) != null) {
+            // Print the content on the console\=
+            String s[];
+            s = strLine.split(" ");
+            switch(s[0].strip())
+            {
+                case(".I"):
+                    //Save last doc and create a new one
+                    if (doc.size() > 0){
+                        //Enter last document into index
+                        addDoc(w, doc);
+                        doc.clear();
+                    }
+                    current_mode = "I";
+                    doc.put("I", s[1].strip());
+                    break;
+                case(".T"):
+                    //Title found
+                    current_mode = "T";
+                    break;
+                case(".A"):
+                    //Author found
+                    current_mode = "A";
+                    break;
+                case(".B"):
+                    //Bibliography found
+                    current_mode = "B";
+                    break;
+                case(".W"):
+                    //Words found
+                    current_mode = "W";
+                    break;
+                default:
+                    if ((current_entry = doc.get(current_mode)) != null){
+                        current_entry += ("\n" + strLine);
+                    }
+                    else{
+                        if(strLine != null){
+                            current_entry = strLine;
+                        }
+                    }
+                    doc.put(current_mode, current_entry);
+                    break;
+            }
+            
+        }
+        if (doc.size() > 0){
+            //Enter last document into index
+            addDoc(w, doc);
+            doc.clear();
+        }
+        //Close the input stream
+        br.close();
+    }
+
+    private static void p(String print_string){
+        System.out.print(print_string + "\n");
+    }
+
+    private static void p(int print_string){
+        System.out.print(print_string + "\n");
+    }
+}
+
---
+++ src/main/java/com/dl/a_1/run_queries.java
@@ -0,0 +1,179 @@
+package com.dl.a_1;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.StopFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.document.Document;
+
+import org.apache.lucene.index.DirectoryReader;
+
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.queryparser.classic.QueryParserBase;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similarities.BooleanSimilarity;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class run_queries {
+    private static String INDEX_DIRECTORY = "index";
+    private static String TREC_FILE = "/cran/results";
+    public static void main(String[] args) throws IOException, ParseException {
+
+        //--------------- SET UP INDEX ----------------------
+        Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
+		
+		// create objects to read and search across the index
+		DirectoryReader ireader = DirectoryReader.open(directory);
+        IndexSearcher isearcher = new IndexSearcher(ireader);
+        //isearcher.setSimilarity(new BooleanSimilarity());
+        //isearcher.setSimilarity(new Similarity());
+        isearcher.setSimilarity(new BM25Similarity());
+
+
+
+        //Analyzer analyzer = new StandardAnalyzer();
+
+        EnglishAnalyzer analyzer = new EnglishAnalyzer();
+        //Analyzer analyzer = new StandardAnalyzer();
+
+        QueryParser parser = new QueryParser("words", analyzer);
+        
+        //---------------- HANDLE QUERY -------------------------
+        //Clear results file
+        PrintWriter pw = new PrintWriter(System.getProperty("user.dir") + "/"+ TREC_FILE);
+        pw.close();
+        
+        
+
+        String query_path = "/Users/daniellavelle/Documents/MAI/CS7IS3/a_1/cran/cran.qry";
+        FileInputStream fstream = new FileInputStream(query_path);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+        String strLine, current_entry;
+        String current_mode = "I";
+        Map<String, String> doc = new HashMap<>();        
+        
+
+        int zero_query_counter = 0;
+        int counter = 0;
+        while ((strLine = br.readLine()) != null) {
+            String s[];
+            s = strLine.split(" ");
+            switch(s[0].strip())
+            {
+                case(".I"):
+                    if (doc.size() > 0){
+
+                        //---- PERFORM QUERY AND ENTER RESULTS - -------- - - -- --- - -- --- ------ - -- -- - -- - -  - - - - --  - -- -- -  -
+                        counter++;
+                        zero_query_counter += process_query(counter, doc, isearcher, parser) ? 0 : 1;
+
+                        doc.clear();
+                    }
+                    current_mode = "I";
+                    doc.put("I", s[1].strip());
+                    break;
+                
+                case(".W"):
+                    current_mode = "W";
+                    break;
+                default:
+                    if ((current_entry = doc.get(current_mode)) != null){
+                        current_entry += ("\n" + strLine);
+                    }
+                    else{
+                        if(strLine != null){
+                            current_entry = strLine;
+                        }
+                    }
+                    doc.put(current_mode, current_entry);
+                    break;
+            }
+            
+        }
+        if (doc.size() > 0){
+            //Enter last document into index
+            //Make last query;
+            zero_query_counter += process_query(counter+ 1, doc, isearcher, parser)? 0 : 1;
+            doc.clear();
+
+        }
+        System.out.print("Num queries processed: " + (counter + 1) + "\nNum zero queries: " + zero_query_counter);
+            
+        
+        //Close the input stream
+        br.close();
+        
+        ireader.close();
+        
+    }
+
+
+    private static Boolean process_query(int query_num, Map<String, String> doc, IndexSearcher isearcher, QueryParser parser) throws IOException, ParseException{
+        FileWriter fw = new FileWriter(System.getProperty("user.dir") + "/"+ TREC_FILE, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter pw = new PrintWriter(bw);
+
+        String querystr = doc.get("W");
+        querystr = QueryParserBase.escape(querystr).trim();
+        Query query = parser.parse(querystr);
+        /*
+        if (Integer.parseInt(doc.get("I")) == 1){
+            System.out.println(querystr);
+            System.out.println(query.toString());
+        }
+        */
+        
+        ScoreDoc[] hits = isearcher.search(query, 1400).scoreDocs;  
+    
+        int numTotalHits = hits.length;
+        
+        int end = Math.min(numTotalHits, 1400);
+        if (end == 0){
+            System.out.print("Zero hit found");
+            pw.close();
+            return false;
+        }
+        for (int i = 0; i < end; i++) {
+            Document d = isearcher.doc(hits[i].doc);
+            String docno = d.get("id");
+            
+
+            write_to_eval(pw, query_num + " Q0 "+docno+" "+( i + 1) +" "+hits[i].score+" "+"0");
+        }
+        pw.close();
+        return true;
+        
+
+    }
+
+    public static void write_to_eval(PrintWriter pw, String string_to_add) throws IOException{
+       
+        pw.println(string_to_add);
+    }
+}
+
